1b7b74aba0007462b408d66268ee2d11e5cceb74
Matheus <matheusaguiar028@gmail.com>
2025-10-27 21:32:51 -0300

    Refactor code structure for improved readability and maintainability

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,21 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve renderizar dois comentários", () => {
+    const { debug } = render(<PostComment />);
+    fireEvent.change(screen.getByTestId('text-comentario'), { target: { value: 'estudar React' } })
+    fireEvent.click(screen.getByTestId('btn-comentar'))
+    fireEvent.change(screen.getByTestId('text-comentario'), { target: { value: 'estudar Java' } })
+    fireEvent.click(screen.getByTestId('btn-comentar'))
+    debug()
+    expect(screen.getByText("estudar React")).toBeInTheDocument();
+    expect(screen.getByText("estudar Java")).toBeInTheDocument();
+  });
 });
